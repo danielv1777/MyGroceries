228cb8ea615982471e319219be542c1a9ab513f0
daniel <danielv1777@yahoo.com>
2020-09-10 14:50:07 -0700

    updated main to be in line with requirements

--- main.cpp
+++ main.cpp
@@ -25,6 +25,18 @@ int main()
         list.push_back(item);
     }
   }while( input != 'q' && input != 'Q' );
-    
+
+  if(list.size() == 0)
+  {
+    cout << "There are no items on the list." << endl;
+  }
+  else
+  {
+    for(int i = 0; i < list.size(); i++)
+    {
+      cout << i+1 << ") " << list[i] << endl;
+    }
+  }
+
   return 0;
 }
